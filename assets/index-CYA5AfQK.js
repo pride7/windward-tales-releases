4cae6d0da0febe8f97d540b042f0b7707744b9e9
Windward Releases <releases@users.noreply.github.com>
2026-09-19 03:50:44 +0000

    web: 0.2.0-ota2 (7c4a6b7)

---
+++ assets/index-CYA5AfQK.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-D68S8rzs.js","./index-DVm3ILD7.js","./index-0OhYyIEA.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-DVm3ILD7.js";const _=r("Share",{web:()=>t(()=>import("./web-D68S8rzs.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.ShareWeb)});export{_ as Share};
