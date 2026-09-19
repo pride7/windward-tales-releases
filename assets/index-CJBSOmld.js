123f7a1a93521001c05dff6887a72a100ede1afd
Windward Releases <releases@users.noreply.github.com>
2026-09-19 05:41:50 +0000

    web: 0.2.0-ota8 (8a61b37)

---
+++ assets/index-CJBSOmld.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-BHYtGeSo.js","./index-CdDDxc5d.js","./index-DJiG1sIT.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-CdDDxc5d.js";const _=r("Share",{web:()=>t(()=>import("./web-BHYtGeSo.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.ShareWeb)});export{_ as Share};
--- assets/index-CMmezei3.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-CfYgCs7j.js","./index-CCKroKUn.js","./index-DJiG1sIT.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-CCKroKUn.js";const _=r("Share",{web:()=>t(()=>import("./web-CfYgCs7j.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.ShareWeb)});export{_ as Share};
