14e6c9707735a215f13e1585dce68034f7ea4578
Windward Releases <releases@users.noreply.github.com>
2026-09-20 22:11:09 +0000

    web: 0.5.0-ota1 (a80744a)

--- sw.js
+++ sw.js
@@ -8,19 +8,19 @@
  * or when the player presses 立即更新. Without that message the waiting worker would sit there forever:
  * reloading the page does not release the old worker, so "takes over next launch" was never actually true.
  */
-const CACHE = 'windward-0.5.0-db7dcd338b9c';
+const CACHE = 'windward-0.5.0-ota1-2c30b1361121';
 const ASSETS = [
-  "./assets/ArenaScene-B4xXa--R.js",
-  "./assets/index-BAo_3R-Z.js",
-  "./assets/index-BCyoPKEG.css",
-  "./assets/index-Cd9ruuPq.js",
-  "./assets/index-DbbP4Tdy.js",
+  "./assets/ArenaScene-BWPYYfvx.js",
+  "./assets/index-C3tXmKBo.js",
+  "./assets/index-CO5ZMDN4.js",
+  "./assets/index-CxQmUpkV.css",
+  "./assets/index-DBALl_Sy.js",
   "./assets/phaser-B8p8Giq7.js",
   "./assets/phaser-DFK5Ua9d.js",
-  "./assets/web-CP9XaH6i.js",
-  "./assets/web-CPPueLm6.js",
-  "./assets/web-D3p1pbyx.js",
-  "./assets/web-Dq1cKG5B.js",
+  "./assets/web-1LvQ5_u3.js",
+  "./assets/web-BVP6M3mw.js",
+  "./assets/web-D4692aw_.js",
+  "./assets/web-DKHaVPNy.js",
   "./favicon.svg",
   "./guide/builds.html",
   "./guide/endgame.html",
