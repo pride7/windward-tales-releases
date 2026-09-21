ac2bf899ec3965b16e8f835ba1ca61729caf931f
Windward Releases <releases@users.noreply.github.com>
2026-09-21 04:02:18 +0000

    web: 0.5.0-ota4 (40aa8ed)

---
+++ assets/index-Bf8aeJGb.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-Cd8Npbc8.js","./index-CMWLHicF.js","./index-Q9wntC_8.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-CMWLHicF.js";const _=r("Share",{web:()=>t(()=>import("./web-Cd8Npbc8.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.ShareWeb)});export{_ as Share};
--- sw.js
+++ sw.js
@@ -8,19 +8,19 @@
  * or when the player presses 立即更新. Without that message the waiting worker would sit there forever:
  * reloading the page does not release the old worker, so "takes over next launch" was never actually true.
  */
-const CACHE = 'windward-0.5.0-ota3-0270f290298f';
+const CACHE = 'windward-0.5.0-ota4-394e65e393b0';
 const ASSETS = [
-  "./assets/ArenaScene-BjmKZEDb.js",
-  "./assets/index-Bbfh3gLp.css",
-  "./assets/index-CbDDlila.js",
-  "./assets/index-Cs8KLnMn.js",
-  "./assets/index-D-mtr80L.js",
+  "./assets/ArenaScene-B_IfvBW0.js",
+  "./assets/index-Bf8aeJGb.js",
+  "./assets/index-CMWLHicF.js",
+  "./assets/index-CQKBseTm.js",
+  "./assets/index-Q9wntC_8.css",
   "./assets/phaser-B8p8Giq7.js",
   "./assets/phaser-DFK5Ua9d.js",
-  "./assets/web-CRb2JZ68.js",
-  "./assets/web-D1pq64yC.js",
-  "./assets/web-D5ucIC5J.js",
-  "./assets/web-DBberzOS.js",
+  "./assets/web-AUPtuMen.js",
+  "./assets/web-BOATg1c_.js",
+  "./assets/web-CQH_9KUd.js",
+  "./assets/web-Cd8Npbc8.js",
   "./favicon.svg",
   "./guide/builds.html",
   "./guide/endgame.html",
