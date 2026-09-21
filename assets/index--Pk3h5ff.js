f820ada5e9308a35dda569af504903b0f4fa4198
Windward Releases <releases@users.noreply.github.com>
2026-09-21 08:15:36 +0000

    web: 0.5.0-ota6 (bccad35)

--- assets/index--Pk3h5ff.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-BRC0bVek.js","./index-BGK1vsdK.js","./index-BaH54iTZ.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-BGK1vsdK.js";const _=r("Share",{web:()=>t(()=>import("./web-BRC0bVek.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.ShareWeb)});export{_ as Share};
---
+++ assets/index-Bo8dq21g.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-zm_Jzycf.js","./index-CY99RF_X.js","./index-B3seU172.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-CY99RF_X.js";const _=r("Share",{web:()=>t(()=>import("./web-zm_Jzycf.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.ShareWeb)});export{_ as Share};
